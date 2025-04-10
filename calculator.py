333bb44264ffed20bacc6515ed8fec698633bf54
Case Zumbrum <casezumbrum@gmail.com>
2025-04-10 11:09:46 -0400

    create blank functions

--- calculator.py
+++ calculator.py
@@ -4,8 +4,29 @@ calculator.py
 
 One function per operation, in order.
 """
-# First example
-def add(a, b): 
+
+import math
+
+
+def add(a, b):
     pass
 
 
+def sub(a, b):
+    pass
+
+
+def mul(a, b):
+    pass
+
+
+def div(a, b):
+    pass
+
+
+def log(a, b):
+    pass
+
+
+def exp(a, b):
+    pass
